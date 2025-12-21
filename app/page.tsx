93a2a4a50a573146c49328a96085915d94c7f362
mytrusathi <mytrusathi@gmail.com>
2025-12-21 14:52:49 +0530

    new website

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,38 @@
 'use client'
 
-import { UserPlus} from 'lucide-react';
-import Navbar from "../components/Navbar";
-import Footer from '../components/Footer'
+import Navbar from "../components/Navbar"
 import Hero from '../components/Hero'
 import WhyChoose from '../components/WhyChoose'
 import FeaturedProfiles from '../components/FeaturedProfiles'
 import GroupAdminCTA from '../components/GroupAdminCTA'
+import Footer from '../components/Footer'
 
 export default function TrusathiUI() {
 
   return (
-    <div className="min-h-screen bg-gray-50 font-sans text-gray-800">
+    <>
+    
+     {/* Top Navigation */}
       <Navbar />
+
+      {/* Hero Section + Search */}
       <Hero />
-      {/* --- Spacer for Search Widget --- */}
-      <div className="h-24 bg-gray-50"></div>
 
-     <WhyChoose />
-     <FeaturedProfiles />
-    <GroupAdminCTA />
-    <Footer />
+      {/* Spacer for floating search */}
+      <div className="h-24 bg-gray-50" />
 
-    </div>
-  );
+      {/* Why Choose Trusathi */}
+      <WhyChoose />
+
+      {/* Featured / Verified Profiles */}
+      <FeaturedProfiles />
+
+      {/* Group Admin Call-To-Action */}
+      <GroupAdminCTA />
+
+      {/* Footer */}
+      <Footer />
+    </>
+   
+  )
 }
